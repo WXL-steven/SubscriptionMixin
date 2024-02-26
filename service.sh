91a8cb355139656c0879cd8e720b4ebf7db2a7c4
WXL-steven <76638078+WXL-steven@users.noreply.github.com>
2024-02-26 17:34:27 +0800

    update auto start sh

--- service.sh
+++ service.sh
@@ -6,7 +6,10 @@
 # export VARIABLE_NAME=value
 
 # 激活 Python 环境
-source /home/web-app/miniforge3/envs/sub/bin/activate
+#source /home/web-app/miniforge3/envs/sub/bin/activate
+
+# Modify to the directory of your FastAPI app
+cd /home/web-app/app/SubscriptionMixin || exit
 
 # 启动 Uvicorn 服务器
-exec uvicorn main:app --host 127.0.0.1 --port 23334
+exec /home/web-app/miniforge3/envs/sub/bin/python -m uvicorn main:app --host 127.0.0.1 --port 23334
